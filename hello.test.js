5c01916fbbe14cc0b8050ffcd37d51b169c7285c
Luis Rodriguez <luis.rodriguez74@gmail.com>
2024-01-15 17:59:02 -0500

    nuevo mensaje

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hola Mundo, desde Lima y en dia de vacaciones!");
+    expect(hello.hello()).toEqual("Hola Mundo, desde Lima y en dia de vacaciones");
   });
 });
